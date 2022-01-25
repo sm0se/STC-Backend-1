0b4e5f5d25f4ca3fa551ef64a6f97eb752d3af89
Mehul <67681485+sm0se@users.noreply.github.com>
2022-01-25 19:43:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const express = require('express')
 const app = express()
 
-app.get('/', function (req, res) {
-    const name = req.query.name
-    res.send("Your name is " + name)
+app.get('/', function (req, res) { 
+    const name = req.query.name      // input name
+    res.send("Your name is " + name) // prints the name defined from the url
 })
 
 app.get("/sum", function (req, res) {
-    const num1 = req.query.num1;
+    const num1 = req.query.num1;    //input number 
     const num2 = req.query.num2;
     const num3 = req.query.num3;
-    const sum = Number(num1) + Number(num2) + Number(num3) // Converting String inputs from the user to number
-    res.send("The average of the numbers is " + sum / 3)
+    const sum = Number(num1) + Number(num2) + Number(num3) // converts string to number
+    res.send("The average of the numbers is " + sum / 3)  
 })
 
 const port = 3000
